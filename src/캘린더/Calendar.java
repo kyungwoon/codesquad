769dcc375e2c36bb56f25115243e80744f87f739
Kyungwoon Na <nakw0410@gmail.com>
2021-11-19 16:39:33 +0900

    Update elaborate calendar

--- src/캘린더/Calendar.java
+++ src/캘린더/Calendar.java
@@ -21,14 +21,34 @@ public class Calendar {
 		}
 	}
 
-	public void printSampleCalendar(int year, int month) {
-		int monthDay = getMaxDaysOfMonth(year,month);
+	public void printSampleCalendar(int year, int month, int week) {
 		System.out.println("      " + year + "년 " + month + "월");
 		System.out.println("  일 월  화  수 목  금 토");
 		System.out.println("---------------------");
-		for (int i = 1; i <= monthDay; i++) {
+		for(int i = 0; i < week; i++) {
+			System.out.print("   ");
+		}
+		
+		//print first line
+		int monthDay = getMaxDaysOfMonth(year,month);
+		int count = 7 - week;
+		int secondLine = count;
+		if(count < 7) {
+			secondLine = count;
+		}else {
+			secondLine = 0;
+		}
+		
+		
+		for(int i = 1; i <= count; i++) {
 			System.out.printf("%3d", i);
-			if (i % 7 == 0) {
+		}
+		System.out.println();
+		count++;
+		//print second line to last
+		for (int i = count; i <= monthDay; i++) {
+			System.out.printf("%3d", i);
+			if (i % 7 == secondLine) {
 				System.out.println();
 			}
 		}
--- src/캘린더/Prompt.java
+++ src/캘린더/Prompt.java
@@ -3,6 +3,31 @@ package 캘린더;
 import java.util.Scanner;
 
 public class Prompt {
+	/**
+	 * 
+	 * @param week 요일명
+	 * @return 0~6 (0 == Sunday, 6 == Saturday)
+	 */
+
+	public int weekDay(String week) {
+		if (week.equals("su")) {
+			return 0;
+		} else if (week.equals("mo")) {
+			return 1;
+		} else if (week.equals("tu")) {
+			return 2;
+		} else if (week.equals("we")) {
+			return 3;
+		} else if (week.equals("th")) {
+			return 4;
+		} else if (week.equals("fr")) {
+			return 5;
+		} else if (week.equals("sa")) {
+			return 6;
+		} else {
+			return 0;
+		}
+	}
 
 	private final static String PROMPT = "cal> ";
 
@@ -11,21 +36,28 @@ public class Prompt {
 		Scanner sc = new Scanner(System.in);
 		Calendar cal = new Calendar();
 
-		int input = 0;
+		int month = 0;
 		int year = 0;
+		int week = 0;
 		while (true) {
-			System.out.println("년도를 입력하세요.");
+			System.out.println("년도를 입력하세요. (-1입력 시 종료)");
 			System.out.print(PROMPT);
 			year = sc.nextInt();
+			if (year == -1) {
+				break;
+			}
 			System.out.println("월을 입력하세요.");
 			System.out.print(PROMPT);
-			input = sc.nextInt();
-			if (input == -1) {
-				break;
-			} else if (input > 12) {
+			month = sc.nextInt();
+			System.out.println("요일을 입력하세요.(su,mo,tu,we,th,fr,sa)");
+			String strweekday = sc.next();
+			week = weekDay(strweekday);
+			if (month > 12 || month < 1) {
+				System.out.println("잘못된 입력입니다.");
 				continue;
 			}
-			cal.printSampleCalendar(year,input);
+
+			cal.printSampleCalendar(year, month, week);
 			System.out.println();
 
 		}
